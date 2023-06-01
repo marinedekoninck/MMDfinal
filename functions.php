6dc6ae111a0ae33a6426bbf4c2e0bc67f481cc80
marinedekoninck <98811917+marinedekoninck@users.noreply.github.com>
2023-06-02 00:31:43 +0200

    style.css and functions.php final versions
    Styling and functions of my Bagberry child theme.

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+	$parenthandle = 'bagberry-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+	$theme        = wp_get_theme();
+	wp_enqueue_style( $parenthandle,
+		get_template_directory_uri() . '/style.css',
+		array(),  // If the parent theme code has a dependency, copy it to here.
+		$theme->parent()->get( '1.0.0' )
+	);
+	wp_enqueue_style( 'bagberrychildtheme-style',
+		get_stylesheet_uri(),
+		array( $parenthandle )
+	);
+}
